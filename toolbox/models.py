fab9891ee7ffd1ca390b103c42715de910c70137
Thomas Lemberger <thomas.lemberger@embo.org>
2020-03-28 01:08:32 +0100

    no grad but no masking of input

--- toolbox/models.py
+++ toolbox/models.py
@@ -22,16 +22,15 @@ class PartiaLConv2d (nn.Conv2d):
     def forward(self, input, mask=None):
         assert input.dim() == 4
         self.mask = mask
-        input = input * self.mask
         if self.mask is not None:
             self.mask_for_input = self.mask.repeat(1, input.size(1), 1, 1) # same number of features as input
-            # with torch.no_grad(): # try removing this
-            W = self.ones.to(input) # to move to same cuda device as input when necessary
-            self.mask_for_output = self.mask_conv(self.mask_for_input, W, bias=None, padding=self.padding, stride=self.stride)
-            self.ratio = self.n / (self.mask_for_output + 1e-8)
-            self.mask_for_output = self.mask_for_output.clamp(0, 1)
-            self.ratio = self.ratio * self.mask_for_output
-
+            with torch.no_grad(): # try removing this
+                W = self.ones.to(input) # to move to same cuda device as input when necessary
+                self.mask_for_output = self.mask_conv(self.mask_for_input, W, bias=None, padding=self.padding, stride=self.stride)
+                self.ratio = self.n / (self.mask_for_output + 1e-8)
+                self.mask_for_output = self.mask_for_output.clamp(0, 1)
+                self.ratio = self.ratio * self.mask_for_output
+             # input = input * self.mask # THIS IS WHAT IS KILLING IT
             output = super().forward(input)
             bias_view = self.bias.view(1, self.out_channels, 1, 1)
             # output = ((output - bias_view) * self.ratio) + bias_view
@@ -60,16 +59,15 @@ class PartialTransposeConv2d(nn.ConvTranspose2d):
     def forward(self, input, mask=None):
         assert input.dim() == 4
         self.mask = mask
-        input = input * self.mask
         if self.mask is not None:
             self.mask_for_input = self.mask.repeat(1, input.size(1), 1, 1) # same number of features as input
-            # with torch.no_grad():
-            W = self.ones.to(input) # to move to same cuda device as input when necessary
-            self.mask_for_output = self.mask_conv(self.mask_for_input, W, bias=None, padding=self.padding, stride=self.stride)
-            self.ratio = self.n / (self.mask_for_output + 1e-8)
-            self.mask_for_output = self.mask_for_output.clamp(0, 1)
-            self.ratio = self.ratio * self.mask_for_output
-
+            with torch.no_grad():
+                W = self.ones.to(input) # to move to same cuda device as input when necessary
+                self.mask_for_output = self.mask_conv(self.mask_for_input, W, bias=None, padding=self.padding, stride=self.stride)
+                self.ratio = self.n / (self.mask_for_output + 1e-8)
+                self.mask_for_output = self.mask_for_output.clamp(0, 1)
+                self.ratio = self.ratio * self.mask_for_output
+            # input = input * self.mask
             output = super().forward(input)
             bias_view = self.bias.view(1, self.out_channels, 1, 1)
             # output = ((output - bias_view) * self.ratio) + bias_view
