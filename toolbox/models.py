848925af5a3dac76bb1536b3f2271f5c675b0568
Thomas Lemberger <thomas.lemberger@embo.org>
2020-04-02 01:08:18 +0200

    not transpose conv, only interpolate, like NVIDIA paper

--- toolbox/models.py
+++ toolbox/models.py
@@ -274,9 +274,9 @@ class Unet(nn.Module):
             # if self.hp.pool:
             #     y = self.unpool(y, indices, 2, stride=2, output_size=list(y_size)) # list(y_size) is to fix a bug in torch 1.0.1; not need in 1.4.0
 
-        y = self.dropout(y)
-        y = self.conv_up(y)
-        y = self.BN_up(F.elu(y, inplace=True))
+        # y = self.dropout(y)
+        # y = self.conv_up(y)
+        # y = self.BN_up(F.elu(y, inplace=True))
         y = F.interpolate(y, x.size(-1), mode='nearest') # hm will not work in 1d
         y = torch.cat((x, y), 1)
         y = self.reduce(y)
