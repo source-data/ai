398531602a9af82275282363cd33dd3c0b44be57
Thomas Lemberger <thomas.lemberger@embo.org>
2020-04-02 21:01:54 +0200

    removing dropout on up branch of unet

--- toolbox/models.py
+++ toolbox/models.py
@@ -275,7 +275,7 @@ class Unet(nn.Module):
             if self.hp.pool:
                 y = self.unpool(y, indices, 2, stride=2, output_size=list(y_size)) # list(y_size) is to fix a bug in torch 1.0.1; not need in 1.4.0
 
-        y = self.dropout(y)
+        # y = self.dropout(y) # optional
         y = self.conv_up(y)
         y = self.BN_up(F.elu(y, inplace=True))
         # y = F.interpolate(y, x.size(-1), mode='nearest')
