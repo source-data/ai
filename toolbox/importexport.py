dd3a0ac9ec7a0c364c5dc191f7111462e6902896
Thomas Lemberger <thomas.lemberger@embo.org>
2020-04-28 10:42:22 +0200

    import export to /tmp

--- toolbox/importexport.py
+++ toolbox/importexport.py
@@ -2,6 +2,7 @@
 #T. Lemberger, 2018
 
 import os
+import io
 import pickle
 import torch
 from torch import nn
@@ -50,7 +51,7 @@ def export_model(model: nn.Module, path: str, filename: str) -> str:
         print(e)
     return archive_path
 
-def unzip_model(model_path: str) -> Tuple[str, str]:
+def hp_model_path(myzip: ZipFile) -> Tuple[str, str]:
     """
     A small utility to unzip a saved model archive.
 
@@ -61,18 +62,16 @@ def unzip_model(model_path: str) -> Tuple[str, str]:
         hp_path, model_path (str, str): the path to the pickeld Hyperparameter object and the save model state.
     """
 
-    with ZipFile(model_path) as myzip:
-        myzip.extractall()
-        for filename in myzip.namelist():
-            _, ext = os.path.splitext(filename)
-            if ext == '.th':
-                model_path = filename
-            elif ext == '.pickle':
-                hp_path = filename
+    for filename in myzip.namelist():
+        _, ext = os.path.splitext(filename)
+        if ext == '.th':
+            model_path = filename
+        elif ext == '.pickle':
+            hp_path = filename
     return hp_path, model_path
 
 
-def load_model_from_class(path:str, model_class: ClassVar, **kwargs) -> nn.Module:
+def load_model_from_class(path: str, model_class: ClassVar, **kwargs) -> nn.Module:
     """
     Generic function to load a model of type model_class.
 
@@ -84,20 +83,24 @@ def load_model_from_class(path:str, model_class: ClassVar, **kwargs) -> nn.Modul
     """
 
     print(f"\n\nloading {path}\n\n")
-    hp_path, model_path = unzip_model(path)
-    with open(hp_path, 'rb') as hyperparams:
-        hp = pickle.load(hyperparams)
+    with ZipFile(path, mode='r') as z:
+        hp_path, model_path = hp_model_path(z)
+        with z.open(hp_path, 'r') as hyperparams:
+            hp = pickle.load(hyperparams)
+        # with z.open(model_path, mode='r') as state_file: # works only on python > 3.7.2 because ZipExtFile needs to implement seek() and tell()
+        z.extract(model_path, '/tmp')
+        model_path = os.path.join('/tmp', model_path)
+        with open(model_path, 'rb') as state_file:
+            state_dict = torch.load(state_file)
+        os.remove(model_path)
     print(f"trying to build model {model_class.__name__} with hyperparameters:")
     print(hp)
-    model =  model_class(hp, **kwargs)
-    state_dict = torch.load(model_path)
+    model = model_class(hp, **kwargs)
     model.load_state_dict(state_dict)
-    os.remove(model_path)
-    os.remove(hp_path)
     return model
 
 
-def load_container(path: str, container: ClassVar) -> Container:
+def load_container(path: str, container: ClassVar, sub_module: ClassVar) -> Container:
     """
     A function to load a Container model.
 
@@ -111,16 +114,21 @@ def load_container(path: str, container: ClassVar) -> Container:
     """
 
     print(f"\n\nloading {path}\n\n")
-    hp_path, model_path = unzip_model(path)
-    with open(hp_path, 'rb') as hyperparams:
-        hp = pickle.load(hyperparams)
+    with ZipFile(path, mode='r') as z:
+        hp_path, model_path = hp_model_path(z)
+        with z.open(hp_path, 'r') as hyperparams:
+            hp = pickle.load(hyperparams)
+        # with z.open(model_path, mode='r') as state_file: # works only on python > 3.7.2 because ZipExtFile needs to implement seek() and tell()
+        z.extract(model_path, '/tmp')
+        model_path = os.path.join('/tmp', model_path)
+        with open(model_path, 'rb') as state_file:
+            state_dict = torch.load(state_file)
+        os.remove(model_path)
     print(f"trying to build model ({container.__name__} with {sub_module.__name__}) with hyperparameters:")
     print(hp)
-    model =  container(hp, sub_module)
-    state_dict = torch.load(model_path)
+    model = container(hp, sub_module)
+
     model.load_state_dict(state_dict)
-    os.remove(model_path)
-    os.remove(hp_path)
     return model
 
 def load_autoencoder(path, sub_module_class: ClassVar = CatStack1d):
@@ -161,7 +169,7 @@ def self_test():
 
 
         hpcs = HyperparametersCatStack(N_layers=2, kernel=7, padding=3, stride=1, in_channels=1, out_channels=1, hidden_channels=2, dropout_rate=0.1)
-        a1dcs = Autoencoder1d(hpcs)
+        a1dcs = Autoencoder1d(hpcs, CatStack1d)
         y = a1dcs(x)
         saved_path = export_model(a1dcs, '/tmp/test', 'test_3')
         print(f"saved {saved_path}")
